9fc211b51f28bc8964d5a3100ff9e14cc97306a9
Carlos romero <soportecarlosromero@gmail.com>
2025-11-30 20:43:22 -0500

    initial creation

---
+++ infinito-security/src/test/java/com/infinitosoft/infinitosecurity/InfinitoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitosoft.infinitosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinitoSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
